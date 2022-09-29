4ac78d68c1a21b198055a2c3b889b36365fd897b
masudosmanefgt <masud.osman@efgt.eurofoods-bd.com>
2022-09-30 03:39:15 +0600

    code cleaned

--- authServer.js
+++ authServer.js
@@ -12,11 +12,8 @@ var refreshTOkens = []
 app.post('/token',(req,res)=>{
     const refreshtoken = req.body.token;
     if(!refreshtoken == null) return res.sendStatus(401);
-    console.log('refresh token',refreshtoken);
-    console.log('refresh token includes',refreshTOkens,refreshTOkens.includes(refreshtoken));
     if(!refreshTOkens.includes(refreshtoken)) return res.sendStatus(403);
     jwt.verify(refreshtoken,process.env.REFRESH_TOKEN_SECRET,(err,user)=>{
-        console.log('refreshtoken verified',err,user);
         if(err) return res.sendStatus(403);
         let temp = { username: user.username, userAge: user.userAge}
         const accesstoken = generateAccessToken(temp);
@@ -43,7 +40,6 @@ app.delete('/logout',(req,res)=>{
 
 function generateAccessToken(user){
     const accesstoken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn:'30s'});
-    console.log('new genreated accesstoken 232',user,accesstoken);
     return accesstoken;
 }
 
--- server.js
+++ server.js
@@ -26,9 +26,7 @@ function authenticateToken(req,res,next){
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1];
     if(token == null) return res.sendStatus(401);
-    console.log('token verify from 3000/ ::',token)
     jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, (err, user)=>{
-        console.log('jwt verify from 3000/::',err, user)
         if(err) return res.sendStatus(403)
         req.user = user;
         next();
